507bdb01edcf2bcb600983208c0369b85eae7233
Logan Born <lcborn4@gmail.com>
2019-08-05 16:39:38 -0400

    Update CharactersController.js
    changed error checking

--- api/controllers/CharactersController.js
+++ api/controllers/CharactersController.js
@@ -10,26 +10,26 @@ module.exports = {
     sails.log("creating character - req.body", req.body);
 
     //error checking
-    if (!req.body.name || !req.body.charClass) {
-      return res.redirect("/dashboard");
-    } else {
+    if (req.body.name && req.body.charClass) {
       //if you don't include fetch - createdCharacter will be undefined
       let createdCharacter = await Characters.create(req.body).fetch();
 
       sails.log(createdCharacter);
-
-      //return to dashboard
-      return res.redirect("/dashboard");
     }
+    //return to dashboard
+    return res.redirect("/dashboard");
   },
   optimize: async function(req, res, next) {
-    sails.log("optimize", req.params);
+    sails.log.debug("optimize params", req.params);
+    sails.log.error("optimize body", req.body);
 
-    let optimizedCharacter = await Characters.update(
-      { id: req.params.id },
-      { optimized: true }
-    );
-
+    //error checking
+    if (req.body.optimizeAttribute) {
+      let optimizedCharacter = await Characters.update(
+        { id: req.params.id },
+        { optimized: true }
+      );
+    }
     //return to dashboard
     return res.redirect("/dashboard");
   }
