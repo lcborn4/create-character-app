a7de92f81dcb3da61e814e3a8309caac3ed767bb
Logan Born <lcborn4@gmail.com>
2019-08-12 13:56:59 -0400

    Update CreateCharacterController.js

--- api/controllers/CreateCharacterController.js
+++ api/controllers/CreateCharacterController.js
@@ -7,35 +7,63 @@
 
 //Main Controller
 
+var axios = require("axios");
+
 module.exports = {
+  //start app
+  start: async function(req, res, next) {
+    sails.log.silly("Start");
 
-    //start app
-    start: async function (req, res, next) {
+    let getI = await getItems();
 
-        sails.log('Start')
+    //phase 1
+    //retrieve items from database
+    let items = await Items.find({});
 
-        //phase 1
-        //retrieve items from database
-        let items = await Items.find({})
+    //retrieve characters from database
+    let characters = await Characters.find({});
+    sails.log.verbose("characters length", characters.length);
 
-        //retrieve characters from database
-        let characters = await Characters.find({})
-        sails.log('characters length',characters.length)
-        
-        //phase 2
-        //retrieve items from outside api
-        //store in database
-        //removing duplicates
-        //retrieve items from database
-        //retrieve characters from database
+    //phase 2
+    //retrieve items from outside api
+    //store in database
+    //removing duplicates
+    //retrieve items from database
+    //retrieve characters from database
 
+    sails.log.silly("Routing to dashboard");
+    sails.log.silly("Characters: ", characters);
+    //return the dashboard
+    res.view("./pages/dashboard", { characters: characters });
 
-
-
-
-        //return the dashboard
-        res.view('./pages/dashboard',{characters: characters})
-    },
-
+    // })
+  }
 };
 
+function getItems() {
+  axios
+    .get(sails.config.globals.itemsAPI)
+    .then(res => {
+      // handle success
+      sails.log.debug(res.data);
+      return res.data;
+    })
+    .catch(function(error) {
+      // handle error
+      sails.log.error(error);
+    })
+    .finally(function() {
+      // always executed
+    });
+}
+
+function compareItems() {
+  sails.log.silly("Comparing Items");
+  //check in the database
+  //send new items to new array
+
+  //write the new items to the database
+
+  //return nothing
+
+}
