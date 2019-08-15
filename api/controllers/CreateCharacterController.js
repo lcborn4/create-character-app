15d31761d5573128063da5582b572d603df49ef3
Logan Born <lcborn4@gmail.com>
2019-08-15 11:56:50 -0400

    added the promises

--- api/controllers/CreateCharacterController.js
+++ api/controllers/CreateCharacterController.js
@@ -14,22 +14,25 @@ module.exports = {
   start: async function(req, res, next) {
     sails.log.silly("Start");
 
-    let getI = await getItems();
-
-    //phase 1
+    /**** phase 1 ****/
     //retrieve items from database
-    let items = await Items.find({});
+    // let items = await Items.find({});
+
+    //retrieve characters from database
+    // let characters = await Characters.find({});
+    // sails.log.verbose("characters length", characters.length);
+
+    /**** phase 2 ****/
+    //retrieve items from outside api
+    let getI = await getItems(); //these should include all items
+
+    //store in database
+    //removing duplicates
+    let compareI = await compareItems();
 
     //retrieve characters from database
     let characters = await Characters.find({});
     sails.log.verbose("characters length", characters.length);
-
-    //phase 2
-    //retrieve items from outside api
-    //store in database
-    //removing duplicates
-    //retrieve items from database
-    //retrieve characters from database
 
     sails.log.silly("Routing to dashboard");
     sails.log.silly("Characters: ", characters);
@@ -41,29 +44,40 @@ module.exports = {
 };
 
 function getItems() {
-  axios
-    .get(sails.config.globals.itemsAPI)
-    .then(res => {
-      // handle success
-      sails.log.debug(res.data);
-      return res.data;
-    })
-    .catch(function(error) {
-      // handle error
-      sails.log.error(error);
-    })
-    .finally(function() {
-      // always executed
-    });
+  return new Promise((resolve, reject) => {
+    axios
+      .get(sails.config.globals.itemsAPI)
+      .then(res => {
+        // handle success
+        sails.log.debug(res.data);
+        resolve(res.data);
+      })
+      .catch(err => {
+        // handle error
+        sails.log.error(err);
+        reject(err);
+      });
+  });
 }
 
+//save the items in the database
+//if already in the database ignore
+//upsert in mongo
 function compareItems() {
   sails.log.silly("Comparing Items");
-  //check in the database
-  //send new items to new array
 
-  //write the new items to the database
+  return new Promise((resolve, reject) => {
+    //check in the database
+    //upset
+    //send new items to new array
 
-  //return nothing
+    //write the new items to the database
 
+    //return nothing
+    resolve();
+  }).catch(err => {
+    // handle error
+    sails.log.error(err);
+    reject(err);
+  });
 }
